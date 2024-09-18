b13a6ad8915749d27d3a78939af0d272abe667a9
dea kayla <dea.kayla10@gmail.com>
2024-09-18 16:01:17 +0700

    menambahkan file lagi

--- board/__init__.py
+++ board/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Hello, World!"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+
+from board import pages
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(pages.bp)
+
+    return app
